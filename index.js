03ecc4b970336d69ede4aacc5c41177a95b5f5b9
HunterHarvell <hunterharvell95@gmail.com>
2022-11-29 14:06:44 -0600

    add write to file and init function

--- index.js
+++ index.js
@@ -127,10 +127,22 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err)
+            throw err;
+        console.log('Information has been transfered to the README.')
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
 
 // Function call to initialize app
 init();
