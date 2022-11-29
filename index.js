6304e32e2893e083034a3eba4902ca1e239ec31d
HunterHarvell <hunterharvell95@gmail.com>
2022-11-29 12:25:09 -0600

    added required packages and first prompts

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // Project name
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the project you are working on? (required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('You need to enter a title to continue!');
+                return false;
+            }
+        }
+    },
+    // Description of project
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You need to provide a project description!');
+                return false;
+            }
+        }
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
