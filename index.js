fa2111801acdecd06b6a0ddfb487cf4152dd0bd5
HunterHarvell <hunterharvell95@gmail.com>
2022-11-29 12:46:00 -0600

    adds questions to the JS

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'What is the title of the project you are working on? (required)',
+        message: 'What is the title of the project? (required)',
         validate: titleInput => {
             if (titleInput) {
                 return true;
             } else {
-                console.log('You need to enter a title to continue!');
+                console.log('You need to enter a title to continue');
                 return false;
             }
         }
@@ -23,17 +23,107 @@ const questions = [
     {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)',
+        message: 'Provide a description of the project. (required)',
         validate: descriptionInput => {
             if (descriptionInput) {
                 return true;
             } else {
-                console.log('You need to provide a project description!');
+                console.log('You need to provide a project description');
                 return false;
             }
         }
     },
-
+    // Installation Instructions
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your project? (required)',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('You need to provide installation info to continue');
+                return false;
+            }
+        }
+    },
+    // Usage Information
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this project? (required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('You need to provide information on how to use project');
+                return false;
+            }
+        }
+    },
+    // Contribution Guidlines
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How should people contribute to this project? (required)',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('You need to provide information on how to contribute to the project');
+                return false;
+            }
+        }
+    },
+    // Test Instructions 
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'How do you test this project? (required)',
+        validate: testingInput => {
+            if (testingInput) {
+                return true;
+            } else {
+                console.log('You need to describe how to test this project');
+                return false;
+            }
+        }
+    },
+    // License Options
+    {
+        type: 'checkbox',
+        name: 'licensing',
+        message: 'Choose a license for your project (required)',
+        choices: ['Apache', 'MIT', 'Mozilla-Public', 'GNU-General-Public', 'Common-Development-and Distribution', 'None'],
+        validate: licensingInput => {
+            if (licensingInput) {
+                return true;
+            } else {
+                console.log('You must pick a license for the project');
+                return false;
+            }
+        }
+    },
+    // Github Username
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username (required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username');
+                return false;
+            }
+        }
+    },
+    // Email Address
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Would you like to include your email?',
+    },
 ];
 
 // TODO: Create a function to write README file
